67f300606390c4d6a2354774748c72373c304471
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-09 01:26:17 +0000

    betty again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 #include <unistd.h>
 #include <string.h>
 /**
-* main - Entry point
-* Return: Always 1 (Success)
-*/
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-int s = strlen("and that piece of art is useful
-\" - Dora Korpar, 2015-10-19\n");
+int s = strlen("and that piece of art is useful"
+"\" - Dora Korpar, 2015-10-19\n");
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 return (1);
 }

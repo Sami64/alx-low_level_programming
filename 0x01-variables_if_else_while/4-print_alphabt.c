19ac444b40d5288b4267ad56108d2b6434bb18cf
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-09 23:34:43 +0000

    aplhp

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 char c;
-for(c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
-if (c != 'q' || c != 'e')
+if (c == 'q' || c == 'e')
 {
-putchar(c);
+continue;
 }
+putchar(c);
 }
 putchar('\n');
 return (0);

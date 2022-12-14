f7714afe9f55ab56197e5b01fb14456db8554b5f
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-14 02:27:40 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - print last digit of a number
+* @n: integer to check
+* Return: 0
+*/
+
+int print_last_digit(int n)
+{
+int digit = n % 10;
+return (digit);
+}

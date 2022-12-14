62f751e53a811afbaff2fdac4f13ff0c7372abe3
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-14 02:41:17 +0000

    minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer
+* Return: 0
+*/
+
+void jack_bauer(void)
+{
+int count = 0;
+int a = 0, b = 0, c = 0, d = 0;
+
+while (count < 1440)
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+
+d++;
+if (d > 9)
+{
+d = 0;
+c++;
+}
+if (c > 5)
+{
+c = 0;
+b++;
+}
+if (b > 9)
+{
+b = 0;
+a++;
+}
+count++;
+}
+}

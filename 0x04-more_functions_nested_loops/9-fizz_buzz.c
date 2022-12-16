be0687c9d6d613b7aec3e37c1c2d5db83eeb2827
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-16 04:32:25 +0000

    buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,21 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)
+i = 1;
+while (i <= 100)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
-continue;
-}
-if (i % 3 == 0)
-{
-printf("Fizz ");
-continue;
-}
-if (i % 5 == 0)
-{
-printf("Buzz ");
-continue;
-}
-printf("%d ", i);
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+
+if (i != 100)
+printf(" ");
+i++;
 }
 printf("\n");
 

2387d81dab88175aede7b150f2506578b0017ef2
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-16 03:31:01 +0000

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - prints 0 - 14 10 times
+* Return: 0
+*/
+
+void more_numbers(void)
+{
+int i;
+int count = 0;
+
+while (count < 10)
+{
+for (i = 0; i < 15; i++)
+{
+_putchar(i + '0');
+}
+count++;
+_putchar('\n');
+}
+}

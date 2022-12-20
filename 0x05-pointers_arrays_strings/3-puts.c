a736a00d3793feb5d8357e797ed16b6dbfdc467b
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-20 05:20:00 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str: pointer to string
+* Return: 0
+*/
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+}

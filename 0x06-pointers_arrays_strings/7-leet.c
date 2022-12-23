73d65ff41b634b7350b6858fd3bcfdabbfb1a15f
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-23 04:08:51 +0000

    str

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* leet - encodes a string into 1337
+* @str: chars
+* Return: str
+*/
+
+char *leet(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+while (str[i] == 'a' || str[i] == 'A')
+str[i] = '4';
+while (str[i] == 'e' || str[i] == 'E')
+str[i] = '3';
+while (str[i] == 'o' || str[i] == 'O')
+str[i] = '0';
+while (str[i] == 't' || str[i] == 'T')
+str[i] = '7';
+while (str[i] == 'l' || str[i] == 'L')
+str[i] = '1';
+}
+
+return (str);
+}

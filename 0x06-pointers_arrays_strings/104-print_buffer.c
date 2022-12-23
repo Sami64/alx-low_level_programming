48374463872ce57f83e64598566689948d2a764e
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-23 05:06:55 +0000

    inf

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_buffer - prints a buffer
+* @b: pointer to buffer
+* @size: size of buffer
+* Return: 0
+*/
+
+void print_buffer(char *b, int size)
+{
+int i, j, k, l;
+char *a = "0123456789abcdef";
+
+}

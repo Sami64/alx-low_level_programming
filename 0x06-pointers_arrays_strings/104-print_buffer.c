78ee9dd530c9f159186e896d016ae0b86b97ff39
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-23 05:14:44 +0000

    buffer print

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,7 +10,26 @@
 
 void print_buffer(char *b, int size)
 {
-int i, j, k, l;
-char *a = "0123456789abcdef";
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        /* Print the position of the first byte of the line in hexadecimal */
+        if (i % 10 == 0)
+            printf("%08x: ", i);
 
+        /* Print the hexadecimal content of the buffer, 2 bytes at a time */
+        printf("%02x ", b[i]);
+
+        /* Print a space every 10 bytes */
+        if (i % 10 == 9)
+            putchar(' ');
+
+        /* Print a new line every 20 bytes */
+        if (i % 20 == 19)
+            putchar('\n');
+    }
+
+    /* Print a new line if there are less than 20 bytes left */
+    if (size % 20 != 0)
+        putchar('\n');
 }

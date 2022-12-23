379ac221d0e3303b316f2602608893b7aa894e68
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-23 04:45:47 +0000

    print int 2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,43 @@
 
 void print_number(int n)
 {
-unsigned int i;
+int power;
+int neg;
+int hold;
 
+neg = 0;
+power = 1;
+hold = n;
 if (n < 0)
 {
 _putchar('-');
-i = -n;
+neg = 1;
 }
-else
+
+while (hold > 9 || hold < -9)
 {
-i = n;
+power *= 10;
+hold /= 10;
+}
+
+while (power > 0)
+{
+if (power > 9)
+{
+if (!neg)
+_putchar((n / power % 10) + '0');
+else
+_putchar((n / power % 10) * -1 + '0');
+
+power /= 10;
+}
+if (power == 1)
+{
+if (neg)
+_putchar((n % 10) * -1 + '0');
+else
+_putchar(n % 10 + '0');
+power = 0;
+}
 }
 }

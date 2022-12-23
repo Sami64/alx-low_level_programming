871ba87adb5e003785c0a348761bc6ebbcd67dae
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-12-23 03:05:20 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 int i = 0;
 int j = 0;
 
-while (dest[i] != '\0')
-{
-i++;
-}
-while (src[j] != '\0' && j < n)
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
 {
 dest[i] = src[j];
-
-i++;
-j++;
 }
-dest[i] = '\0';
 
 return (dest);
 }

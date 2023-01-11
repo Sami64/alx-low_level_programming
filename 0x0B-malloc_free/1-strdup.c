1fd842cd8c697e63a15ecc04acbd2380189514af
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-11 05:43:22 +0000

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,22 +13,21 @@
 
 char *_strdup(char *str)
 {
-char *arr;
+char *a;
+int i, c;
 
 if (str == NULL)
 return (NULL);
-arr = malloc(sizeof(str));
-if (arr == NULL)
-return (NULL);
-else
-{
-unsigned int i = 0;
-while (str[i] != '\0')
-{
 
-arr[i] = str[i];
-i++;
-}
-}
-return (arr);
+for (i = 0; str[i] != '\0'; i++)
+;
+
+a = malloc(i * sizeof(*a) + 1);
+if (a == NULL)
+return (NULL);
+
+for (c = 0; c < i; c++)
+a[c] = str[c];
+a[c] = '\0';
+return (a);
 }

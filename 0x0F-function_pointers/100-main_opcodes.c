72865ee4b8e64147a573506c63331efb194e24e7
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-19 06:01:37 +0000

    calc ops

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 if (argc != 2)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 int bytes = atoi(argv[1]);
 if (bytes < 0)
 {
 printf("Error\n");
-return 2;
+return (2);
 }
-unsigned char* main_ptr = (unsigned char*) main;
+unsigned char *main_ptr = (unsigned char *) main;
 for (int i = 0; i < bytes; i++)
 {
 printf("%02x", main_ptr[i]);
 }
 printf("\n");
-return 0;
+return (0);
 }

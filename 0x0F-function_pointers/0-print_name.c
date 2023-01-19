4a090561d01e95c7c6c52f54f2dee2fb0d1035d4
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-19 05:33:57 +0000

    func pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_name - prints a name
+* @name: name to print
+* @f: function pointer to print name
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}

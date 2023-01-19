1a9d4d4909882cd61f90be80187c2d54f238696b
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-19 06:00:08 +0000

    calc ops

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return 1;
+}
+int bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+return 2;
+}
+unsigned char* main_ptr = (unsigned char*) main;
+for (int i = 0; i < bytes; i++)
+{
+printf("%02x", main_ptr[i]);
+}
+printf("\n");
+return 0;
+}

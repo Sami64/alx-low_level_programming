3d993b20df54308c3cd77c12a17125c479ad9746
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-19 23:10:36 +0000

    variadic one unsigned

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,7 +11,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
 unsigned int i;
-int sum = 0;
+unsigned int sum = 0;
 
 va_start(ap, n);
 for (i = n; i >= 0; i = va_arg(ap, unsigned int))

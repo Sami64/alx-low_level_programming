5dafd6d49a30fcd623134f8754ed57ffa6903101
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-27 06:04:47 +0000

    0 fix free

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,24 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-const list_t *temp;
-size_t c;
+size_t nodes = 1;
 
-c = 1;
-temp = h;
 if (h == NULL)
 return (0);
 
-while (temp->next != NULL)
+while (h->next != NULL)
 {
-if (temp->str == NULL)
+if (h->str == NULL)
 printf("[%d] %s\n", 0, "(nil)");
 else
-printf("[%d] %s\n", temp->len, temp->str);
-temp = temp->next;
-c++;
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+nodes++;
 }
-printf("[%d] %s\n", temp->len, temp->str);
-
-return (c);
+printf("[%d] %s\n", h->len, h->str);
+return (nodes);
 }

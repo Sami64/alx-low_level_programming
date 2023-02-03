9f080cd9cd952cc32131bfe1ed8fadc2058b2490
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-02-03 04:59:18 +0000

    all unsigned betty

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,3 @@
-//
-// Created by Samuel Kow Paintsil on 03/02/2023.
-//
-
 #ifndef ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
 #define ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
 
@@ -9,4 +5,4 @@
 
 unsigned int binary_to_uint(const char *b);
 
-#endif //ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+#endif
